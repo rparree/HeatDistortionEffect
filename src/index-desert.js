9cc63bcb6063f58fcb0c660940acedf6d1269c75
Raphael Parree <rparree@edc4it.com>
2020-05-01 10:15:03 +0200

    made heat animation stoppable/restartable

--- src/index-desert.js
+++ src/index-desert.js
@@ -1,8 +1,7 @@
-import {createCanvas} from './utils/canvas';
 import Haze from './haze';
 import shader from './shaders/haze-desert.frag';
 
-let canvas=document.querySelector('.background__canvas');
+let canvas = () => document.querySelector('.background__canvas');
 
 let textureAlign={x:0.9,y:0.9};
 let textures=[
@@ -54,7 +53,7 @@ function loadContent(src,canvas,scroll=0){
   }
 
   let titleImage=new Image();
-  titleImage.addEventListener('load',event=>{
+  titleImage.addEventListener('load',()=>{
     let dpi=haze.dpi;
     canvas.width=titleImage.width*dpi;
     canvas.height=titleImage.height*dpi;
@@ -75,129 +74,123 @@ function loadContent(src,canvas,scroll=0){
   titleImage.src=src;
 
   return value;
-};
+}
 
-let contents=[
+let contents = () => [
   loadContent(
-    'img/desert-title.svg',
-    document.querySelector('.background__title--big')
+      'img/desert-title.svg',
+      document.querySelector('.background__title--big')
   ),
   loadContent(
-    'img/desert-title-medium.svg',
-    document.querySelector('.background__title--medium')
+      'img/desert-title-medium.svg',
+      document.querySelector('.background__title--medium')
   ),
   loadContent(
-    'img/desert-title-small.svg',
-    document.querySelector('.background__title--small')
+      'img/desert-title-small.svg',
+      document.querySelector('.background__title--small')
   ),
 ]
 
-let getContent=(function(){
-  let lastContent={};
-  let wasLoaded=false;
-  let lastww=0;
-  return function(){
-    let ww=window.innerWidth;
-    let content=(ww!=lastww)
-      ?
-      contents.reduce(
-        (last,content)=>window.innerWidth<=content.breakpoint?content:last
-      )
-      :
-      lastContent;
+let getContent = function () {
+  let lastContent = {};
+  let wasLoaded = false;
+  let lastww = 0;
+  return function () {
+    let ww = window.innerWidth;
+    let content = (ww !== lastww)
+        ?
+        contents().reduce(
+            (last, content) => window.innerWidth <= content.breakpoint ? content : last
+        )
+        :
+        lastContent;
 
     lastww=ww;
 
-    if((content==lastContent && content.loaded!=wasLoaded) || content!=lastContent){
+    // if((content==lastContent && content.loaded!=wasLoaded) || content!=lastContent){
       haze.gl.createUniform('1i','content',content.textureIndex);
       haze.gl.createTexture(content.canvas,content.textureIndex);
-      haze.gl.createUniform('1i','contentLoaded',content.loaded?1:0);
-      haze.gl.createUniform('2f','contentPos',0,0);
-      haze.gl.createUniform('2f','contentSize',content.canvas.width,content.canvas.height);
+    haze.gl.createUniform('1i', 'contentLoaded', content.loaded ? 1 : 0);
+    haze.gl.createUniform('2f', 'contentPos', 0, 0);
+    haze.gl.createUniform('2f', 'contentSize', content.canvas.width, content.canvas.height);
 
-      lastContent=content;
-      wasLoaded=content.loaded;
-      return content;
+    lastContent = content;
+    wasLoaded = content.loaded;
+    return content;
 
-    }else return lastContent;
+    //}else return lastContent;
   }
-}());
-function updateContent(){
-  let content=getContent();
-  if(content.loaded){
-    let title=content.canvas;
-    let bb=title.getBoundingClientRect();
-    haze.gl.createUniform(
-      '2f','contentPos',
-      bb.left*haze.dpi,
-      (bb.top+
-        (content.scroll!=0?document.body.scrollTop*content.scroll:0)
-      )*haze.dpi
-    );
-  }
+};
+
+function updateContent() {
+  let content = getContent();
+  // if(content.loaded){
+  let title = content.canvas;
+  let bb = title.getBoundingClientRect();
+  haze.gl.createUniform(
+      '2f', 'contentPos',
+      bb.left * haze.dpi,
+      (bb.top +
+          (content.scroll !== 0 ? document.body.scrollTop * content.scroll : 0)
+      ) * haze.dpi
+  );
+  //}
 }
-let haze=new Haze({
-  canvas,
-  shader,
-  textures,
-});
+
+let haze // lazy
 
 
-haze.gl.createUniform('2f','mouse',0.5,0.5);
-
-const smooth=(n=6)=>{
-  let samples=[];
-  return (v)=>{
-    samples=samples.concat(v);
-    if(samples.length>n){
-      samples=samples.slice(samples.length-n,samples.length);
+const smooth = (n = 6) => {
+  let samples = [];
+  return (v) => {
+    samples = samples.concat(v);
+    if (samples.length > n) {
+      samples = samples.slice(samples.length - n, samples.length);
     }
-    return samples.reduce((l,cur)=>(l+cur))/samples.length;
+    return samples.reduce((l, cur) => (l + cur)) / samples.length;
   }
 }
 
-const curve=(v,p=0.8)=>v==0?0:Math.pow(Math.abs(v),p)*(v/Math.abs(v));
+const curve=(v,p=0.8)=>v===0?0:Math.pow(Math.abs(v),p)*(v/Math.abs(v));
 
 let smoothX=smooth();
 let smoothY=smooth();
 
-var isTouchDevice = 'ontouchstart' in document.documentElement;
+const isTouchDevice = 'ontouchstart' in document.documentElement;
 
-window.addEventListener('mousemove',function(event){
-  if(!isTouchDevice){
-    haze.gl.createUniform('2f','mouse',
-      -curve(-1+((event.pageX/window.innerWidth)*2)),
-      -curve(-1+((event.pageY/window.innerHeight)*2))
+window.addEventListener('mousemove',function(event) {
+  if (!active) return;
+  if (!isTouchDevice) {
+    haze.gl.createUniform('2f', 'mouse',
+        -curve(-1 + ((event.pageX / window.innerWidth) * 2)),
+        -curve(-1 + ((event.pageY / window.innerHeight) * 2))
     );
   }
 });
 
-window.addEventListener('devicemotion',function(event){
-  if(isTouchDevice){
-    haze.gl.createUniform('2f','mouse',
-      curve(smoothX(-event.accelerationIncludingGravity.x/10))*12,
-      curve(smoothY(-event.accelerationIncludingGravity.y/10))*3
+window.addEventListener('devicemotion',function(event) {
+  if (!active) return;
+  if (isTouchDevice) {
+    haze.gl.createUniform('2f', 'mouse',
+        curve(smoothX(-event.accelerationIncludingGravity.x / 10)) * 12,
+        curve(smoothY(-event.accelerationIncludingGravity.y / 10)) * 3
     );
   }
 });
 
 
-haze.gl.createUniform('1i','noiseSize',256);
-
-haze.addEventListener('predraw',function(){
-  haze.gl.createUniform("2f","noiseR",Math.round(Math.random()*256),Math.round(Math.random()*256));
-  haze.gl.createUniform("2f","noiseG",Math.round(Math.random()*256),Math.round(Math.random()*256));
-  haze.gl.createUniform("2f","noiseB",Math.round(Math.random()*256),Math.round(Math.random()*256));
-})
-function getDPI(){
-  if(typeof window.devicePixelRatio!="undefined"){
+function getDPI() {
+  if (typeof window.devicePixelRatio != "undefined") {
     return window.devicePixelRatio;
-  }else{
+  } else {
     return 1;
   }
 }
-// window.addEventListener('resize',updateSize);
-function start(){
+
+window.addEventListener('resize', updateSize);
+let active = false
+
+function updateSize(){
   let container=document.querySelector('.background');
   let dimensions=container.getBoundingClientRect();
   haze.width=dimensions.width;
@@ -207,7 +200,67 @@ function start(){
   haze.gl.createUniform('2f','resolution',haze.width*haze.dpi,haze.height*haze.dpi);
   updateContent();
 }
-window.webyarns_dessert = {
-  start: start
+
+function start(containerId) {
+  active = true;
+  defineCanvasStructure(containerId)
+  haze = new Haze({
+    canvas: canvas(),
+    shader,
+    textures,
+  });
+  haze.gl.createUniform('2f', 'mouse', 0.5, 0.5);
+  haze.gl.createUniform('1i', 'noiseSize', 256);
+
+  haze.addEventListener('predraw', function () {
+    haze.gl.createUniform("2f", "noiseR", Math.round(Math.random() * 256), Math.round(Math.random() * 256));
+    haze.gl.createUniform("2f", "noiseG", Math.round(Math.random() * 256), Math.round(Math.random() * 256));
+    haze.gl.createUniform("2f", "noiseB", Math.round(Math.random() * 256), Math.round(Math.random() * 256));
+  })
+
+
+
+  updateSize();
 }
-// updateSize();
+
+function stop(containerId) {
+  active = false;
+  const c = document.getElementById(containerId);
+  while (c.lastElementChild) {
+    c.removeChild(c.lastElementChild);
+  }
+}
+
+function defineCanvasStructure(containerId) {
+  const container = document.getElementById(containerId);
+  if (container.childElementCount === 0) {
+
+    const bg = document.createElement("background__canvas")
+    bg.setAttribute("class", "background")
+
+    const bgc = document.createElement("canvas")
+    bgc.setAttribute("class", "background__canvas")
+
+
+    const bgtb = document.createElement("canvas")
+    bgtb.setAttribute("class", "background__title background__title--big")
+
+    const bgtm = document.createElement("canvas")
+    bgtm.setAttribute("class", "background__title background__title--medium")
+
+    const bgts = document.createElement("canvas")
+    bgts.setAttribute("class", "background__title background__title--small")
+
+    bg.appendChild(bgc)
+    bg.appendChild(bgtb)
+    bg.appendChild(bgtm)
+    bg.appendChild(bgts)
+    container.appendChild(bg)
+  }
+}
+
+window.webyarns_dessert = {
+  start,
+  stop
+}
+
